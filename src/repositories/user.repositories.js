212dd12c37b739ebea2f134faba17fbf4aab8ada
Fernando <fernando.goncalves@aluno.faculdadeimpacta.com.br>
2025-11-11 09:42:51 -0300

    Create user repository insert

---
+++ src/repositories/user.repositories.js
@@ -0,0 +1,32 @@
+import db from "../config/databse.js";
+
+db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        avatar TEXT
+        )
+    `);
+function createUserRepository(newUser){
+    return new Promise((resolve, reject) =>{
+        const{username, email, password, avatar}=newUser
+        db.run(
+            `
+            INSERT INTO users (username, email, password, avatar)
+            VALUES (?, ?, ?, ?)`,
+            [username,email, password, avatar],
+            (err) => {
+                if(err){
+                    reject(err)
+                } else{
+                    resolve({message: 'User Create'})
+                }
+            }
+        )
+    })
+}
+export default {
+    createUserRepository
+}
